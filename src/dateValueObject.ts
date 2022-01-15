2856ef12429e0f3a418e39e9327cbd3c6a4eb827
LUN7 <kwl188836@gmail.com>
2022-01-16 02:05:04 +0800

    feat: value objects

---
+++ src/dateValueObject.ts
@@ -0,0 +1,19 @@
+import { ValueObject } from "valueObject";
+
+export class DateValueObject extends ValueObject<Date> {
+  static create(value: Date, name: string): DateValueObject {
+    return new DateValueObject({ value, name });
+  }
+
+  protected validate(value: Date, min: Date, max: Date): boolean {
+    return value >= min && value <= max;
+  }
+
+  public isEqual(vo: ValueObject<unknown>): boolean {
+    return vo instanceof DateValueObject && vo.value === this.value;
+  }
+
+  public toString(): string {
+    return this.value.toString();
+  }
+}
